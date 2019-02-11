495841568077b615a5b43ee0b29d5cac578e96eb
Adam Lodowski <adamlodowski@gmail.com>
2019-02-11 11:15:01 +0000

    Passing first user requirement. User can make a deposit

--- lib/Account.rb
+++ lib/Account.rb
@@ -3,4 +3,13 @@ class Account
     @balance = balance
   end
 
+  def deposit(value)
+    @balance += value
+    p "Thank you for depositing £#{value}"
+  end
+
+  def show_balance
+    p "Your balance is £#{@balance}"
+  end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,3 +1,15 @@
 describe Account do
 
+  before(:each) do
+    @account = Account.new
+  end
+
+  it "can accept a user deposit" do
+    expect(@account.deposit(1000)).to eq("Thank you for depositing £1000")
+  end
+
+  it "can show the user's balance" do
+    expect(@account.show_balance).to eq("Your balance is £0")
+  end
+
 end
