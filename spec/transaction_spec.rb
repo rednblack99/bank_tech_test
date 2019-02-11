0504b6c9d80fa66bf0893bdce92fd2e4a49aa76c
Adam Lodowski <adamlodowski@gmail.com>
2019-02-11 12:20:24 +0000

    Improved unit tests for refactor

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,2 +1,21 @@
 describe Transaction do
+
+  context 'can read the' do
+
+    before(:each) do
+      @transaction = Transaction.new(1000, "01/01/2001", true)
+    end
+
+    it 'date' do
+      expect(@transaction.date).to eq "01/01/2001"
+    end
+
+    it 'value' do
+      expect(@transaction.value).to eq 1000
+    end
+
+    it 'debit' do
+      expect(@transaction.debit).to eq true
+    end
+  end
 end
